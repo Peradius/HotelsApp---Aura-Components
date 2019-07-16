5091da72130ba3ba878246c849319d0967a29b47
Wszolek <michal.wszolek@accenture.com>
2019-07-16 16:02:14 +0200

    Initial work with enhanced user interface

--- aura/HotelApp/HotelApp.app
+++ aura/HotelApp/HotelApp.app
@@ -1,4 +1,3 @@
 <aura:application extends="force:slds">
-  <c:searchHotels />
-   <c:guestBooking />
+    <c:searchHotels />
 </aura:application>
--- aura/roomBooking/roomBookingController.js
+++ aura/roomBooking/roomBookingController.js
@@ -4,12 +4,6 @@
 		component.set("v.guest", guest);
 		console.log("Guest Data received!")
 		console.log("Guest name: " + guest.First_Name__c)
-		
-		// We don't care about the room data, because guest data is sent
-		// when a room is selected, therefore we assume the room data
-		// is always here when guest data comes with event
-		component.set("v.receivedAllData", true);
-
 	},
 
 	handleRoomEvent : function(component, event, helper) {
