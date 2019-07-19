4b0c8c78d0d8f1c7a3897d621d10084bea266187
Peradius <peradius@gmail.com>
2019-07-19 13:03:23 +0200

    Added simple user menu

--- aura/userMenu/userMenuController.js
+++ aura/userMenu/userMenuController.js
@@ -1,5 +1,10 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+    handleGuestDataEvent :  function(component, event, helper){
+		console.log("I have received the guest data in user menu");
+		var guest = event.getParam("guest");
+		console.log(guest.First_Name__c);
+		component.set("v.guest", guest);
+		component.set("v.loggedIn", "true");
+		helper.queryReservations(component, guest);
+    }
 })
--- aura/userMenu/userMenuHelper.js
+++ aura/userMenu/userMenuHelper.js
@@ -1,5 +1,23 @@
 ({
-	helperMethod : function() {
-		
+	queryReservations : function(component, guest) {
+		var action = component.get("c.getReservations");
+		action.setParams({
+			"guestId" : guest.Id
+		});
+
+		action.setCallback(this, function(response){
+			var state = response.getState();
+			if (state === "SUCCESS") {
+				var reservations = action.getReturnValue();
+				component.set("v.reservations", reservations);
+				console.log("Reservations retrieved!");
+				console.log("Check in: " + component.get("v.reservations")[0].Check_In__c);
+				console.log("Room Name: " + component.get("v.reservations")[0].Room__r.Name);
+			} else {
+				console.log("Error retrieveing reservations");
+				console.log(response);
+			}
+		});
+		$A.enqueueAction(action);
 	}
 })
